e77d6f1203a9a473d7ef10fdfd8b620a6fbd3fee
klopucki <klopucki@tt.com.pl>
2026-06-16 09:53:55 +0200

    Create dynamic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,30 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Schools } from './pages/schools/schools';
-import { Trainings } from './pages/trainings/trainings';
-import { News } from './pages/news/news';
-import { TrainingDetails } from './pages/training-details/training-details';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
 
-  { path: 'schools', component: Schools },
+  {
+    path: 'schools',
+    loadComponent: () => import('./pages/schools/schools').then((m) => m.Schools),
+  },
 
-  { path: 'trainings', component: Trainings },
-  { path: 'trainings/:id', component: TrainingDetails },
+  {
+    path: 'trainings',
+    loadComponent: () => import('./pages/trainings/trainings').then((m) => m.Trainings),
+  },
+  {
+    path: 'trainings/:id',
+    loadComponent: () =>
+      import('./pages/training-details/training-details').then((m) => m.TrainingDetails),
+  },
 
-  { path: 'news', component: News },
+  {
+    path: 'news',
+    loadComponent: () => import('./pages/news/news').then((m) => m.News),
+  },
 
   { path: '**', redirectTo: '' },
 ];
